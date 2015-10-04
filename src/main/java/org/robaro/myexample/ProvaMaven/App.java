b4f974aecdaafc064e0544a4fb91370068d34a7c
robaro <robaro85@hotmail.it>
2015-10-04 18:52:40 +0200

    Commit di Prova

---
+++ src/main/java/org/robaro/myexample/ProvaMaven/App.java
@@ -0,0 +1,13 @@
+package org.robaro.myexample.ProvaMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
